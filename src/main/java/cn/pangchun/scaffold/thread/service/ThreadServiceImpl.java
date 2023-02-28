699f2529bc3c5a4d1969b08e3b3e59a32209481a
pangchun <1007920179@qq.com>
2023-02-28 23:23:28 +0800

    线程学习

---
+++ src/main/java/cn/pangchun/scaffold/thread/service/ThreadServiceImpl.java
@@ -0,0 +1,19 @@
+package cn.pangchun.scaffold.thread.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * ThreadServiceImpl
+ *
+ * @author pangchun
+ * @since 2023/2/28
+ */
+@Service
+public class ThreadServiceImpl implements ThreadService {
+    @Override
+    public void downloadImg(List<String> urls) {
+
+    }
+}
